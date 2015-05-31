bdce59bdd6c41b3948a32f67fc0ee1a689f101c7
Koji Sekiguchi <koji.sekiguchi@rondhuit.com>
2015-05-31 14:34:34 +0900

    add a sample script for describing how ShingleFilter works

---
+++ 20150603/shinglefilter.scala
@@ -0,0 +1,10 @@
+import org.nlp4l.core.analysis.AnalyzerBuilder
+
+val ab = AnalyzerBuilder()
+
+ab.withTokenizer("standard")
+ab.addTokenFilter("shingle", "minShingleSize", "2", "maxShingleSize", "2", "outputUnigrams", "false")
+
+val analyzer = ab.build()
+
+analyzer.tokens("NLP4L is a Natural Language Processing tool for Lucene").map(_.getOrElse("term", null)).foreach(println(_))
