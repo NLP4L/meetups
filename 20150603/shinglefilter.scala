5aca6f0fc97c8d6641bf41d778179c5c68491f77
Koji Sekiguchi <koji.sekiguchi@rondhuit.com>
2015-06-01 09:58:52 +0900

    add tri-gram

--- 20150603/shinglefilter.scala
+++ 20150603/shinglefilter.scala
@@ -1,11 +1,27 @@
 import org.nlp4l.core.analysis.AnalyzerBuilder
 
-val ab = AnalyzerBuilder()
+val STR = "NLP4L is a Natural Language Processing tool for Lucene"
 
-ab.withTokenizer("standard")
-ab.addTokenFilter("shingle", "minShingleSize", "2", "maxShingleSize", "2", "outputUnigrams", "false")
-ab.addTokenFilter("lowercase")
+// bi-gram
+val ab2 = AnalyzerBuilder()
 
-val analyzer = ab.build()
+ab2.withTokenizer("standard")
+ab2.addTokenFilter("shingle", "minShingleSize", "2", "maxShingleSize", "2", "outputUnigrams", "false")
+ab2.addTokenFilter("lowercase")
 
-analyzer.tokens("NLP4L is a Natural Language Processing tool for Lucene").map(_.getOrElse("term", null)).foreach(println(_))
+val analyzer2 = ab2.build()
+
+println("\n\n--- bi-gram ---")
+analyzer2.tokens(STR).map(_.getOrElse("term", null)).foreach(println(_))
+
+// tri-gram
+val ab3 = AnalyzerBuilder()
+
+ab3.withTokenizer("standard")
+ab3.addTokenFilter("shingle", "minShingleSize", "3", "maxShingleSize", "3", "outputUnigrams", "false")
+ab3.addTokenFilter("lowercase")
+
+val analyzer3 = ab3.build()
+
+println("\n\n--- tri-gram ---")
+analyzer3.tokens(STR).map(_.getOrElse("term", null)).foreach(println(_))
