6a0eee75f792480e77d406343b8bf4ef178134f0
Tomoko Uchida <tomoko.uchida.1111@gmail.com>
2015-06-03 14:34:27 +0900

    Fix scripts.

--- 20150603/04_tfidf.scala
+++ 20150603/04_tfidf.scala
@@ -6,7 +6,15 @@ val schema = SchemaLoader.loadFile("examples/schema/ldcc.conf")
 val reader = IReader(indexDir, schema)
 
 // ドキュメントID 100 のTF文書ベクトルを作る
-TFIDF.tfVector(reader, "body", 100)
+val vec1 = TFIDF.tfVector(reader, "body", 100)
+// ベクトルの素性(単語)一覧
+vec1._1
+// TFベクトル
+vec1._2
 
 // ドキュメントID 100 のTFIDF文書ベクトルを作る
-TFIDF.tfIdfVector(reader, "body", 100)
+val vec2 = TFIDF.tfIdfVector(reader, "body", 100)
+// ベクトルの素性(単語)一覧
+vec2._1
+// TFベクトル
+vec2._2
